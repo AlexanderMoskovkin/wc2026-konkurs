888dc09f7fc23dfe1a612364e8195e433d4823e5
Alexander Moskovkin <amoskovkin88@gmail.com>
2026-09-06 09:17:34 +0300

    site: add Haldei setlist data

---
+++ haldei-setlist/data.js
@@ -0,0 +1,182 @@
+export const SONGS = [
+  {
+    "id": "song-00",
+    "title": "Вступление",
+    "seconds": 94,
+    "bpm": null
+  },
+  {
+    "id": "song-01",
+    "title": "Корабль в бутылке",
+    "seconds": 240,
+    "bpm": 135.0
+  },
+  {
+    "id": "song-02",
+    "title": "Тайны леса",
+    "seconds": 260,
+    "bpm": 167.0
+  },
+  {
+    "id": "song-03",
+    "title": "Новогодняя елка",
+    "seconds": 154,
+    "bpm": 155.0
+  },
+  {
+    "id": "song-04",
+    "title": "Монстрик",
+    "seconds": 200,
+    "bpm": 182.0
+  },
+  {
+    "id": "song-05",
+    "title": "Кошмарник",
+    "seconds": 220,
+    "bpm": 190.0
+  },
+  {
+    "id": "song-06",
+    "title": "Быль и сказка",
+    "seconds": 230,
+    "bpm": 90.0
+  },
+  {
+    "id": "song-07",
+    "title": "Домовой",
+    "seconds": 225,
+    "bpm": 180.0
+  },
+  {
+    "id": "song-08",
+    "title": "Разбойник ночи",
+    "seconds": 247,
+    "bpm": 212.0
+  },
+  {
+    "id": "song-09",
+    "title": "Мертвый капитан",
+    "seconds": 177,
+    "bpm": 93.0
+  },
+  {
+    "id": "song-10",
+    "title": "Халдей",
+    "seconds": 195,
+    "bpm": 165.0
+  },
+  {
+    "id": "song-11",
+    "title": "Бродячий цирк",
+    "seconds": 242,
+    "bpm": 168.0
+  },
+  {
+    "id": "song-12",
+    "title": "Певец-мертвец",
+    "seconds": 231,
+    "bpm": 154.0
+  },
+  {
+    "id": "song-13",
+    "title": "Зеркала",
+    "seconds": 150,
+    "bpm": 141.0
+  },
+  {
+    "id": "song-14",
+    "title": "Граф и скелет",
+    "seconds": 163,
+    "bpm": 149.0
+  },
+  {
+    "id": "song-15",
+    "title": "Голос леса",
+    "seconds": 220,
+    "bpm": 146.0
+  },
+  {
+    "id": "song-16",
+    "title": "Пурга",
+    "seconds": 290,
+    "bpm": 102.0
+  },
+  {
+    "id": "song-17",
+    "title": "Игоша",
+    "seconds": 147,
+    "bpm": 161.0
+  },
+  {
+    "id": "song-18",
+    "title": "Хрустальный бал",
+    "seconds": 275,
+    "bpm": 82.0
+  },
+  {
+    "id": "song-19",
+    "title": "Смотритель маяка",
+    "seconds": 163,
+    "bpm": 163.0
+  },
+  {
+    "id": "song-20",
+    "title": "Яга",
+    "seconds": 227,
+    "bpm": 83.0
+  },
+  {
+    "id": "song-21",
+    "title": "Сказка на ночь",
+    "seconds": 145,
+    "bpm": 192.0
+  },
+  {
+    "id": "song-22",
+    "title": "Хвастун и леший",
+    "seconds": 125,
+    "bpm": 192.0
+  },
+  {
+    "id": "song-23",
+    "title": "Шут и гадалка",
+    "seconds": 234,
+    "bpm": 146.0
+  },
+  {
+    "id": "song-24",
+    "title": "Воры золото хранили",
+    "seconds": 220,
+    "bpm": 141.0
+  }
+];
+
+export const DEFAULT_ORDER = [
+  "song-00",
+  "song-12",
+  "song-13",
+  "song-19",
+  "song-14",
+  "song-01",
+  "song-23",
+  "song-07",
+  "song-02",
+  "song-06",
+  "song-04",
+  "song-21",
+  "song-05",
+  "intermission",
+  "song-09",
+  "song-17",
+  "song-20",
+  "song-15",
+  "song-22",
+  "song-16",
+  "song-08",
+  "song-18",
+  "song-03",
+  "song-24",
+  "song-10",
+  "song-11"
+];
+
